aa9ed25547286baa2034f3bb84699f87462ebcd4
kateriniovi <katrina-rose@windowslive.com>
2022-06-25 09:52:26 +0200

    see comments

--- kate.py
+++ kate.py
@@ -27,15 +27,56 @@ import numpy as np
 
 # 10x : 1022 nm/px (JPK camera) - not sure we can really be precise at the nm for this one.
 # 20x: 506 nm/px (JPK camera)
-
+import PIL
 from PIL import Image
-im = Image.open('20220117_E2_Cell8BF.tif')
+from PIL.TiffTags import TAGS
+im = Image.open('topo_20220107_Exp2Cell8map-data-2022.01.07-16.34.34.510_processed-2022.01.10-12.38.51.tif')
 plt.imshow(im)
+
 pix = np.array(im)
-print(pix)
+
+# meta_dict = {TAGS[key] : im.tag[key] for key in im.tag_v2}
+# meta_dict
+# info_dict = {
+#     "Filename": im.filename,
+#     "Image Size": im.size,
+#     "Image Height": im.height,
+#     "Image Width": im.width,
+#     "Image Format": im.format,
+#     "Image Mode": im.mode,
+#     "Image is Animated": getattr(im, "is_animated", False),
+#     "Frames in Image": getattr(im, "n_frames", 1),
+#     "info": im.info
+# }
+
+# for label,value in info_dict.items():
+#     print(f"{label:25}: {value}")
+
+print(PIL.__version__)
+exifdata = im.getexif()
+
+for tag_id in exifdata:
+    # get the tag name, instead of human unreadable tag id
+    tag = TAGS.get(tag_id, tag_id)
+    data = exifdata.get(tag_id)
+    # decode bytes 
+    if isinstance(data, bytes):
+        data = data.decode()
+    print(f"{tag:25}: {data}")
+    
+for label,value in exif():
+    print(f"{label:25}: {value}")
 
 
+# import tifffile as tiff
+# import cv
+# a = tiff.imread('topo_20220107_Exp2Cell8map-data-2022.01.07-16.34.34.510_processed-2022.01.10-12.38.51.tif')
+# im = cv2.imread('a.tif', -1)
 
+
+# print(pix)
+# meta_dict = {TAGS[key] : im.tag[key] for key in im.tag_v2}
+# print(meta_dict)
 #fjxhfksdh
 
 # import numpy as np
